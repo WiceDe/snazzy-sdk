953c730e257a2367bd3334158908cad98bdb60fb
SvenHoeffler <shoeffler@wice.de>
2021-05-21 13:31:01 +0200

    Added outline for initial functions

---
+++ tests/import.test.js
@@ -0,0 +1,11 @@
+/* eslint consistent-return: "off" */
+/* eslint no-multi-str: "off" */
+/* eslint-disable no-unused-expressions */
+
+const { expect } = require('chai');
+
+describe('Base tests', () => {
+  it('should ensure that test suite is functional', async () => {
+    expect(true).to.be.true;
+  });
+});
